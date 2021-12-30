44b0b3a62b9c6f3ef4a8ab6d94875bd2db477bd0
Srijita <srijitabhattacharjee123@gmail.com>
2021-12-31 01:28:49 +0530

    added autodetect location

--- index.js
+++ index.js
@@ -47,3 +47,17 @@ document.querySelector(".search-bar").addEventListener("keyup",function(event)
   }
 });
 weather.fetchWeather("Kolkata");
+const x = document.getElementById("demo");
+
+function getLocation() {
+  try {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } catch {
+    x.innerHTML = err;
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
